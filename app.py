d9d8ae3b8f915b42811d4c60f5b7964c1ad386bb
adoringonion <adringonion.byazura@gmail.com>
2020-02-29 20:09:16 +0900

    add gensim

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import *
-
+from gensim.models.doc2vec import Doc2Vec
+from flask import Flask, request
 app = Flask(__name__)
+
+model = Doc2Vec.load('doc2_1.model')
 
 
 @app.route('/')
 def index():
-    return jsonify({"aaa": "fsdfsd"})
+    return 'hello world'
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/post", methods=['POST'])
+def post():
+    hoge = request.form['hoge']
+
+    return model.docvecs.most_similar(hoge, topn=10)[0][0]
+
+
+if __name__ == '__main__':
+    # http://localhost:5000/ でアクセスできるよう起動
+    app.run(host='localhost', port=5000)
